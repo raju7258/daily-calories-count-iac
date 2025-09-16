45da036ebf62670908ec5697d63887ba6791d169
Rajesh Murali Nair <rajeshnair@mac.home>
2025-09-16 15:58:56 +0200

    Updating S3 bucket with public access

--- daily_calories_count_ia_c/daily_calories_count_ia_c_stack.py
+++ daily_calories_count_ia_c/daily_calories_count_ia_c_stack.py
@@ -28,7 +28,12 @@ class DailyCaloriesCountIaCStack(Stack):
             website_index_document="index.html",
             website_error_document="index.html",
             public_read_access=True,
-            block_public_access=s3.BlockPublicAccess.BLOCK_ACLS,
+            block_public_access=s3.BlockPublicAccess(
+                block_public_acls=False,
+                block_public_policy=False,
+                ignore_public_acls=False,
+                restrict_public_buckets=False
+            ),
             removal_policy=removal_policy,
             auto_delete_objects=auto_delete,
             cors=[
