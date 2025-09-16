174781c3852fd3d4901996b18e493cc1aef850e6
Rajesh Murali Nair <rajeshnair@mac.home>
2025-09-16 17:33:38 +0200

    Adding Cognito too

--- daily_calories_count_ia_c/daily_calories_count_ia_c_stack.py
+++ daily_calories_count_ia_c/daily_calories_count_ia_c_stack.py
@@ -1,6 +1,7 @@
 from aws_cdk import (
     Stack,
     aws_s3 as s3,
+    aws_cognito as cognito,
     CfnOutput,
     RemovalPolicy,
     Duration,
@@ -27,6 +28,7 @@ class DailyCaloriesCountIaCStack(Stack):
             bucket_name=config["resources"]["s3_bucket_name"],
             website_index_document="index.html",
             website_error_document="index.html",
+            block_public_access=s3.BlockPublicAccess.BLOCK_ALL,
             removal_policy=removal_policy,
             auto_delete_objects=auto_delete,
             cors=[
@@ -38,10 +40,47 @@ class DailyCaloriesCountIaCStack(Stack):
                 )
             ],
         )
+
+        # Cognito User Pool
+        user_pool = cognito.UserPool(
+            self,
+            "UserPool",
+            user_pool_name=f"nourisheye-user-pool-{env_name}",
+            sign_in_aliases=cognito.SignInAliases(email=True),
+            self_sign_up_enabled=True,
+            standard_attributes=cognito.StandardAttributes(
+                email=cognito.StandardAttribute(required=True, mutable=True),
+                given_name=cognito.StandardAttribute(required=True, mutable=True)
+            ),
+            password_policy=cognito.PasswordPolicy(
+                min_length=8,
+                require_lowercase=True,
+                require_uppercase=True,
+                require_digits=True
+            ),
+            account_recovery=cognito.AccountRecovery.EMAIL_ONLY,
+            removal_policy=removal_policy
+        )
+
+        # User Pool Client
+        user_pool_client = cognito.UserPoolClient(
+            self,
+            "UserPoolClient",
+            user_pool=user_pool,
+            user_pool_client_name=f"nourisheye-app-client-{env_name}",
+            generate_secret=False,
+            auth_flows=cognito.AuthFlow(
+                user_srp=True,
+                user_password=True
+            )
+        )
+
         # Apply tags
         for key, value in config["tags"].items():
             Tags.of(self).add(key, value)
             
         CfnOutput(self, f"BucketName-{env_name}", value=web_bucket.bucket_name)
         CfnOutput(self, f"WebsiteURL-{env_name}", value=web_bucket.bucket_website_url)
+        CfnOutput(self, f"UserPoolId-{env_name}", value=user_pool.user_pool_id)
+        CfnOutput(self, f"UserPoolClientId-{env_name}", value=user_pool_client.user_pool_client_id)
 
